087a2a6c7815ccc65a260c791812d98f3567ec71
Jeffrey Cross <geophphrie@gmail.com>
2022-12-21 09:32:01 -0500

    Debug changes to json_cursor for testing

--- querybuilder/cursor.py
+++ querybuilder/cursor.py
@@ -2,6 +2,8 @@ import contextlib
 import json
 import psycopg2
 
+from django.db.backends.utils import CursorDebugWrapper
+
 
 @contextlib.contextmanager
 def json_cursor(django_database_connection):
@@ -12,5 +14,26 @@ def json_cursor(django_database_connection):
     https://www.psycopg.org/docs/extras.html#psycopg2.extras.register_default_jsonb
     """
     with django_database_connection.cursor() as cursor:
-        psycopg2.extras.register_default_jsonb(conn_or_curs=cursor.cursor, loads=json.loads)
+        inner_cursor = cursor.cursor
+        # if hasattr(cursor, 'cursor'):
+        #     inner_cursor = cursor.cursor
+        # Normally cursor is a CursorWrapper with the real cursor as a property. In debug, there is
+        # a second layer wrapper called CursorDebugWrapper, so the thing we want is cursor.cursor.cursor :-o
+
+        # if isinstance(inner_cursor, CursorDebugWrapper):
+        #     print('json_cursor: cursor was CursorDebugWrapper')
+        #     inner_cursor = cursor.cursor
+
+        try:
+            psycopg2.extras.register_default_jsonb(conn_or_curs=inner_cursor, loads=json.loads)
+        except TypeError:
+            print(f'json_cursor: conn_or_curs was actually a {type(inner_cursor)}')
+            if isinstance(inner_cursor, CursorDebugWrapper):
+                inner_cursor = inner_cursor.cursor
+            try:
+                psycopg2.extras.register_default_jsonb(conn_or_curs=inner_cursor, loads=json.loads)
+            except TypeError as e:
+                print(f'json_cursor: conn_or_curs was actually a {type(inner_cursor)}')
+                raise TypeError(e)
+
         yield cursor
